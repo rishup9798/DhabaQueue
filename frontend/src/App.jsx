ea184d21959e308bb63d9843caf4953b5b739775
Rishu Pandey <cutiebebo91@gmail.com>
2026-09-18 02:28:51 +0530

    Fix frontend routing on Render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 import HowItWorksPage from "./pages/HowItWorksPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/how-it-works" element={<HowItWorksPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
